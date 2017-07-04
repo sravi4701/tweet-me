05b1fa1491e4c232f5d7746098cb6ab9ca650cc0
sravi4701 <sravi4701@gmail.com>
2017-07-04 18:46:02 +0530

    template file added

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Tweet(models.Model):
+	user 		= models.ForeignKey(settings.AUTH_USER_MODEL)
 	content 	= models.CharField(max_length=140)
 	updated 	= models.DateTimeField(auto_now=True)
 	timestamp 	= models.DateTimeField(auto_now_add=True)
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tweet
+def tweet_detail_view(request, id=1):
+	obj = Tweet.objects.get(id=id)
+	context = {'object': obj}
+	return render(request, "tweets/detail_view.html", context)
+
+
+def tweet_list_view(request):
+	obj = Tweet.objects.all()
+	context = {'object':obj}
+	return render(request, "tweets/list_view.html", context)
