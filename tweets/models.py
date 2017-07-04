13ebd199b80ae23a8139fca04eef8870efcc75e7
sravi4701 <sravi4701@gmail.com>
2017-07-04 16:15:53 +0530

    update fields of Tweet

--- tweets/models.py
+++ tweets/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Tweet(models.Model):
-	content = models.TextField()
+	content 	= models.CharField(max_length=140)
+	updated 	= models.DateTimeField(auto_now=True)
+	timestamp 	= models.DateTimeField(auto_now_add=True)
+	
 
 	def __str__(self):
 		return self.content
