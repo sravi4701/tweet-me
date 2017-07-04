55d192b5e3d550533d95619371681e25187daa22
sravi4701 <sravi4701@gmail.com>
2017-07-04 15:27:10 +0530

    add view

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home)
 ]
 
 if settings.DEBUG :
