863867f7e09d8a55d02681d589860260687a0cf6
sravi4701 <sravi4701@gmail.com>
2017-07-13 02:05:55 +0530

    Run a simple test

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Tweet
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+class TweetModelTestCase(TestCase):
+
+	def setUp(self):
+		some_random_user = User.objects.create(username="ravi33333333333")
+
+	def test_tweet_item(self):
+		obj = Tweet.objects.create(
+				user=User.objects.first(),
+				content="Some random content"
+			)
+
+		self.assertTrue(obj.content == "Some random content")
+		self.assertTrue(obj.id == 1)
+
+		absolute_url = reverse("tweet:detail", kwargs={"pk":1})
+
+		self.assertTrue(obj.get_absolute_url(), absolute_url)
