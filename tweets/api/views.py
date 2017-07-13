aac99fa17b1fd3cc4a7609e47703c08737b5c1a8
sravi4701 <sravi4701@gmail.com>
2017-07-14 01:14:10 +0530

    Add ajax and jquery search

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
-
+from django.db.models import Q
 from .serializers import TweetModelSerializer
 from tweets.models import Tweet
 class TweetListAPIView(generics.ListAPIView):
 	serializer_class = TweetModelSerializer
 	def get_queryset(self):
-		return Tweet.objects.all()
+		qs = Tweet.objects.all()
+		print(self.request.GET)
+		query = self.request.GET.get("q", None)
+		if query is not None:
+			qs = qs.filter(
+					Q(content__icontains=query) |
+					Q(user__username__icontains=query)
+				)
+
+		return qs
