03e604da040e9d37fc32dd451fc9c58de5e8a900
sravi4701 <sravi4701@gmail.com>
2017-07-13 22:40:12 +0530

    Create new app accounts and foreign key in serializers

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 
 from tweets.models import Tweet
 
+from accounts.api.serializers import UserDisplaySerializer
+
 class TweetModelSerializer(serializers.ModelSerializer):
+	user = UserDisplaySerializer()
 	class Meta:
 		model = Tweet
 		fields = [
