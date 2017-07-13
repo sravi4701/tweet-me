120c5ee26138f8d7454c55b9843976299f8ea4f5
sravi4701 <sravi4701@gmail.com>
2017-07-14 02:43:04 +0530

    display the dates in tweets

--- tweetme/settings/local.py
+++ tweetme/settings/local.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'tweets',
     'rest_framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
