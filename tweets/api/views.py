c825aaf6c40124f5193eed0697d11eb1ef8e961c
sravi4701 <sravi4701@gmail.com>
2017-07-14 18:17:07 +0530

    Add the pagination to the list view

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from django.db.models import Q
 from .serializers import TweetModelSerializer
 from tweets.models import Tweet
+from .pagination import StandardResultPagination
 
 class TweetCreateAPIView(generics.CreateAPIView):
 	serializer_class = TweetModelSerializer
@@ -11,6 +12,7 @@ class TweetCreateAPIView(generics.CreateAPIView):
 
 class TweetListAPIView(generics.ListAPIView):
 	serializer_class = TweetModelSerializer
+	pagination_class = StandardResultPagination
 	def get_queryset(self):
 		qs = Tweet.objects.all().order_by("-timestamp")
 		print(self.request.GET)
