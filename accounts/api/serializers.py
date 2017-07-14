ae181b457c114f7b64f20dc3630edbbef2acfb04
sravi4701 <sravi4701@gmail.com>
2017-07-15 01:40:21 +0530

    Add url to the profile in list view

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,19 +1,25 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.urls import reverse_lazy
 
 User = get_user_model()
 
 class UserDisplaySerializer(serializers.ModelSerializer):
 	follower_count = serializers.SerializerMethodField()
+	url = serializers.SerializerMethodField()
 	class Meta:
 		model = User
 		fields = [
 			'username',
 			'first_name',
 			'last_name',
-			'follower_count'
+			'follower_count',
+			'url'
 		]
 
 	def get_follower_count(self, obj):
 		# print(obj.username)
 		return 0
+
+	def get_url(self, obj):
+		return reverse_lazy("account:detail", kwargs={"username":obj.username})
