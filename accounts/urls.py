5fc112b1c14fd38e27649ebbe60c76cabaaff0ad
sravi4701 <sravi4701@gmail.com>
2017-07-15 19:26:21 +0530

    Add follow button to follow the user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import (UserDetailView)
+from .views import (UserDetailView, UserFollowView)
 
 from django.views.generic import RedirectView
 
@@ -8,6 +8,7 @@ urlpatterns = [
 	# url(r'^$', RedirectView.as_view(url="/")),
 	# url(r'^$', TweetListAPIView.as_view(), name="list"), #api/tweet
 	url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name="detail"),#profile/1
+	url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name="follow"),#profile/1/follow
 	# url(r'^create/$', TweetCreateAPIView.as_view(), name="create"), #tweet/create/
 	# url(r'^(?P<pk>\d+)/edit$', TweetUpdateView.as_view(), name="edit"), #tweet/1/edit
 	# url(r'^(?P<pk>\d+)/delete$', TweetDeleteView.as_view(), name="delete"), #tweet/1/delete
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView
 from django.contrib.auth import get_user_model
-
+from django.views import View
+from accounts.models import UserProfile
 
 User = get_user_model()
 
@@ -14,3 +16,21 @@ class UserDetailView(DetailView):
 
 	def get_object(self, *args, **kwargs):
 		return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
+
+
+
+class UserFollowView(View):
+	def get(self, request, username, *args, **kwargs):
+		toggle_user = get_object_or_404(User, username__iexact=username)
+		if request.user.is_authenticated():
+			# user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+			user_profile = self.request.user.profile
+			if toggle_user in user_profile.following.all():
+				user_profile.following.remove(toggle_user)
+			else:
+				user_profile.following.add(toggle_user)
+
+		return redirect("account:detail", username=username)
+
+		# url = reverse("account:detail")
+		# HttpResponseRedirect(url)
