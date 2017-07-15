7ec76bd751b99225faec82742f701c5c39254470
sravi4701 <sravi4701@gmail.com>
2017-07-15 23:21:23 +0530

    toggle follow/unfollow

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse_lazy
 # Create your models here.
 
 class UserProfileManager(models.Manager):
@@ -15,6 +16,28 @@ class UserProfileManager(models.Manager):
 			pass
 		return qs
 
+	def toggle_follow(self, user, to_toggle_user):
+		user_profile, created = UserProfile.objects.get_or_create(user=user)
+		if to_toggle_user in user_profile.following.all():
+			user_profile.following.remove(to_toggle_user)
+			added = False
+		else:
+			user_profile.following.add(to_toggle_user)
+			added = True
+		return added
+
+	def is_following(self, user, followed_by_user):
+		user_profile, created = UserProfile.objects.get_or_create(user=user)
+		if created:
+			return False
+		if followed_by_user in user_profile.following.all():
+			return True
+
+		return False
+
+
+
+
 class UserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile") # user.profile 
 
@@ -22,7 +45,7 @@ class UserProfile(models.Model):
 	# user.profile.following -- user I follow
 	# user.followed_by -- user that follow me --reverse relationship
 
-	object = UserProfileManager()
+	objects = UserProfileManager()
 
 	def __str__(self):
 		return str(self.following.all().count())
@@ -30,3 +53,9 @@ class UserProfile(models.Model):
 	def get_following(self):
 		users =  self.following.all()
 		return users.exclude(username=self.user.username)
+
+	def get_follow_url(self):
+		return reverse_lazy("account:follow", kwargs={"username":self.user.username})
+
+	def get_absolute_url(self):
+		return reverse_lazy("account:detail", kwargs={"username":self.user.username})
--- accounts/views.py
+++ accounts/views.py
@@ -17,20 +17,22 @@ class UserDetailView(DetailView):
 	def get_object(self, *args, **kwargs):
 		return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
 
-
+	def get_context_data(self, *args, **kwargs):
+		context = super(UserDetailView, self).get_context_data(*args, **kwargs)
+		following = UserProfile.objects.is_following(self.request.user, self.get_object())
+		context["following"] = following
+		return context
 
 class UserFollowView(View):
 	def get(self, request, username, *args, **kwargs):
 		toggle_user = get_object_or_404(User, username__iexact=username)
 		if request.user.is_authenticated():
 			# user_profile, created = UserProfile.objects.get_or_create(user=request.user)
-			user_profile = self.request.user.profile
-			if toggle_user in user_profile.following.all():
-				user_profile.following.remove(toggle_user)
-			else:
-				user_profile.following.add(toggle_user)
+			is_following = UserProfile.objects.toggle_follow(self.request.user, toggle_user)
 
 		return redirect("account:detail", username=username)
 
 		# url = reverse("account:detail")
 		# HttpResponseRedirect(url)
+
+# user_profile = self.request.user.profile
