8ca4bc485eda765b90767809e9d7e1b7b0c4f214
sravi4701 <sravi4701@gmail.com>
2017-07-15 17:39:44 +0530

    Update followers and following count

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+
+	use_for_related_fields = True
+
+	def all(self):
+		qs = self.get_queryset().all()
+		try:
+			if self.instance:
+				qs = qs.exclude(user=self.instance)
+		except:
+			pass
+		return qs
+
 class UserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile") # user.profile 
 
@@ -9,6 +22,11 @@ class UserProfile(models.Model):
 	# user.profile.following -- user I follow
 	# user.followed_by -- user that follow me --reverse relationship
 
+	object = UserProfileManager()
 
 	def __str__(self):
 		return str(self.following.all().count())
+
+	def get_following(self):
+		users =  self.following.all()
+		return users.exclude(username=self.user.username)
